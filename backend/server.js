9c0a406fde49a368674c32b4692fd5a1c06a27d7
Chetan Rayalu <$ chetanrayaluthirumalasetty@gmail.com>
2025-03-26 10:39:31 +0530

    form added

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const OpenAI = require("openai");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
